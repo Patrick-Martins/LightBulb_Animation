65ac8ccf32151abcbbe69bb924b01d0a97064e6a
aviki <toldi.victoria@gmail.com>
2020-03-24 00:58:49 +0100

    added animation to washing-machine

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener("DOMContentLoaded", init);
 
 async function init() {
   fetchSVG();
-  changeViewBox();
+  //changeViewBox();
 }
 
 async function fetchSVG() {
@@ -13,5 +13,53 @@ async function fetchSVG() {
   console.log(mySVGData);
 
   document.querySelector("section.scene1").innerHTML = mySVGData;
-  document.getElementById("room").setAttribute("viewBox", "270 120 100 100");
+  turnOnTheLight();
+  turnOnTheTV();
+  turnOnTheWM();
+
+  //document.getElementById("room").setAttribute("viewBox", "270 120 100 100");
+}
+//Makes the lamp clickable
+function turnOnTheLight() {
+  document.querySelectorAll("#stand-lamp").forEach(element => {
+    element.addEventListener("click", lightOn);
+    console.log(element);
+  });
+}
+
+//Makes the washing-machine clickable --> calls te animation
+function turnOnTheWM() {
+  document.querySelectorAll("#washing-machine").forEach(element => {
+    element.addEventListener("click", wash);
+    console.log(element);
+  });
+}
+
+//Makes the TV clickable --> calls the animation
+function turnOnTheTV() {
+  document.querySelectorAll("#tv").forEach(element => {
+    element.addEventListener("click", tvOn);
+    console.log(element);
+  });
+}
+
+var lampIsOn = true;
+//Displays the light
+function lightOn() {
+  if (lampIsOn) {
+    document.getElementById("light-standlamp").style.display = "none";
+    lampIsOn = false;
+  } else {
+    document.getElementById("light-standlamp").style.display = "block";
+    lampIsOn = true;
+  }
+}
+
+function tvOn() {
+  console.log("hello");
+}
+
+function wash() {
+  var washingMachine = document.getElementById("washing-machine-glass");
+  washingMachine.classList.toggle("wash");
 }

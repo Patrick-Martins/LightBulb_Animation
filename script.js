213cb79061deedc43e4514e0502e3ca405805a94
aviki <toldi.victoria@gmail.com>
2020-03-23 23:07:59 +0100

    get the svg

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+"use strict";
+window.addEventListener("DOMContentLoaded", init);
+
+async function init() {
+  fetchSVG();
+  //changeViewBox();
+}
+
+async function fetchSVG() {
+  let response = await fetch("final-room.svg");
+  let mySVGData = await response.text();
+
+  console.log(mySVGData);
+
+  document.querySelector("section.scene1").innerHTML = mySVGData;
+  document.getElementById("room").setAttribute("viewBox", "270 120 100 100");
+}
+
 function turnOnTheLight() {
   document.querySelectorAll(".cls-87, .cls-88").forEach(element => {
     element.addEventListener("click, lightOn");

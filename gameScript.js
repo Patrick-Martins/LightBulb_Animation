1a19b9f739048c0c3a905abbc54dbad728e1be54
liatalony <liatalony311@gmail.com>
2020-03-24 15:48:36 +0100

    made babbles change color, and lamp click

--- gameScript.js
+++ gameScript.js
@@ -1,23 +1,59 @@
+"use strict";
 window.addEventListener("DOMContentLoaded", init);
 function init() {
-  fetchSVG1();
-  //fetchSVG2();
+  fetchSVG();
+  // animateBubbles();
 }
-
-async function fetchSVG1() {
+let clicked;
+async function fetchSVG() {
   fetch("edisons-first-lamp.svg")
     .then(r => r.text())
-    .then(text => (document.querySelector("section.lamp").innerHTML = text))
+    .then(text => {
+      document.querySelector("section.lamp").innerHTML = text;
+    })
     .then(() => fetch("bubble.svg"))
     .then(r => r.text())
-    .then(text => (document.querySelector("section.bubbles").innerHTML = text));
-  // let mySVGData = await response.text();
-  // document.querySelector("section.lamp").innerHTML = mySVGData;
+    .then(text => {
+      document.querySelector("section.bubbles").innerHTML = text;
+      animateBubbles();
+      editLamp();
+    });
 }
-async function fetchSVG2() {
-  fetch("bubble.svg")
-    .then(r => r.text())
-    .then(text => (document.querySelector("section.bubbles").innerHTML = text));
-  // let SVGData = await res.text();
-  //document.querySelector("section.bubbles").innerHTML = SVGData;
+
+function animateBubbles() {
+  const bubbles = document.querySelectorAll(".bubble");
+  console.log(bubbles);
+
+  bubbles.forEach(adding);
+}
+function adding(bubble) {
+  bubble.addEventListener("click", () => {
+    clearAll();
+    bubble.querySelector(".st1").style.fill = "lightblue";
+    clicked = bubble.id;
+    console.log(clicked);
+  });
+}
+function clearAll() {
+  const bubbles = document.querySelectorAll(".bubble");
+  bubbles.forEach(bubble => {
+    bubble.querySelector(".st1").style.fill = "white";
+  });
+}
+function editLamp() {
+  document.querySelector(".Lampst6").setAttribute("display", "none");
+  document.querySelector("#filament").setAttribute("display", "none");
+
+  document.querySelector("#glass").addEventListener("click", chechMaterial);
+}
+function chechMaterial() {
+  if (clicked == "bamboo") {
+    console.log("That's right!");
+
+    document.querySelector(".Lampst6").setAttribute("display", "static");
+    document.querySelector("#filament").setAttribute("display", "static");
+    document.querySelector("#filament").style.stroke = "orange";
+  } else {
+    console.log("Wrong!");
+  }
 }

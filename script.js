4017f81d02c36ebe5f7b2e72eab9f9c6e252eff9
AndyJ <andr41b0@stud.kea.dk>
2020-03-24 09:36:16 +0100

    modal working and refresh button works

--- script.js
+++ script.js
@@ -3,15 +3,22 @@ window.addEventListener("DOMContentLoaded", init);
 
 async function init() {
   fetchSVG();
-  changeViewBox();
 }
 
 async function fetchSVG() {
   let response = await fetch("final-room.svg");
   let mySVGData = await response.text();
 
-  console.log(mySVGData);
-
   document.querySelector("section.scene1").innerHTML = mySVGData;
   document.getElementById("room").setAttribute("viewBox", "270 120 100 100");
+  clickBook();
+}
+
+function clickBook() {
+  const openModal = document.getElementById("open-book");
+  const modalBg = document.querySelector(".modal-bg");
+  openModal.addEventListener("click", function() {
+    modalBg.classList.remove("hidden");
+    console.log("openModal");
+  });
 }

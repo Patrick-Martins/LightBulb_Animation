4c6d0996120067664e8120004e487d84cc962876
Patrick <patr983e@stud.kea.dk>
2020-03-27 00:02:15 +0100

    reverted branchRevert "house on fire animation"
    This reverts commit e80abe2c5c86a8635d1f748a17aac9cbc536cb11.

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ import { TweenLite } from "gsap/all";
 import { TweenMax } from "gsap/all";
 import { Power1 } from "gsap/all";
 import { Bounce } from "gsap/all";
-import { Elastic } from "gsap/all";
 import { _createElement } from "gsap/CSSPlugin";
 
 ("use strict");
@@ -41,6 +40,7 @@ async function fetchSVG() {
   turnOnTheWM();
   turnOnTheTV();
   turnOnTheSpeaker();
+
   document.getElementById("room").setAttribute("viewBox", "270 120 100 100");
 }
 /****************** STANDLAMP ANIMATION *******************************************************/
@@ -179,14 +179,7 @@ function turnOnTheSpeaker() {
 function fetchAllImages() {
   fetchImage("timeline.svg", ".timeline ");
   //   fetchImage("content_images/house_fire.svg", ".lifestyle-impact");
-  // fetchImage("content_images/house-fire.svg", ".lifestyle-impact");
-}
-
-async function fetchHouse() {
-  let response = await fetch("house-fire.svg");
-  let mySVGData = await response.text();
-  document.querySelector("section.house").innerHTML += mySVGData;
-  animateFlames();
+  fetchImage("content_images/house.svg", ".lifestyle-impact");
 }
 
 async function fetchTimeline() {
@@ -413,8 +406,6 @@ function updateModal() {
       fetchFactory();
     } else if (settings.currentContent == "process-curiosity") {
       fetchLamp();
-    } else if (settings.currentContent == "lifestyle-impact") {
-      fetchHouse();
     }
   }
 }
@@ -600,18 +591,4 @@ function animateFactory() {
     yoyo: true,
     repeat: -1
   });
-  const arrows = document.getElementById("arrows");
-  TweenMax.to(arrows, 0.5, {
-    x: "+=4",
-    yoyo: true,
-    repeat: -1
-  });
-}
-
-/********************** HOUSE ON FIRE ANIMATION ***************************/
-
-function animateFlames() {
-  // const element = document.querySelector(".fire");
-  TweenMax.from(".fire", 1, { scaleY: 0, ease: Elastic.easeOut, delay: 1.5 });
-  TweenLite.to(".fire", 1, { scale: 1.1, repeat: -1, yoyo: true, ease: Power1.easeInOut, delay: 2.5 });
 }
